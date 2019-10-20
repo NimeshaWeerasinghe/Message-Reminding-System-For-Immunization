4e42733c02d7d7ed0b9b6ee6dd11a1f4b7f94594
Nimesha Weerasinghe <nimeshaweerasinghe94@gmail.com>
2019-10-20 10:11:46 +0530

    file

---
+++ src/medical/AgeInfo.java
@@ -0,0 +1,300 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package medical;
+
+/**
+ *
+ * @author Hansi
+ */
+public class AgeInfo {
+    
+    private String babyname;
+    private String mothername;
+    private String tphm;
+    private String tpmob;
+    private boolean sms;
+    private String age;
+    boolean next;
+    private int color;
+    private String select;
+    private String month2sms;
+    private String month4sms;
+    private String month6sms;
+    private String month9sms;
+    private String year1sms;
+    private String month18sms;
+    private String year3sms;
+    private String year5sms;
+    private String year12sms;
+    private String idnum;
+    /**
+     * @return the name
+     */
+    
+
+    
+
+    
+    
+
+   
+
+   
+
+    /**
+     * @return the sender
+     */
+    
+
+    /**
+     * @return the age
+     */
+    public String getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    /**
+     * @param tphm the tphm to set
+     */
+    public void setTphm(String tphm) {
+        this.tphm = tphm;
+    }
+
+    /**
+     * @param tpmob the tpmob to set
+     */
+    public void setTpmob(String tpmob) {
+        this.tpmob = tpmob;
+    }
+
+    /**
+     * @return the tpmob
+     */
+    public String getTpmob() {
+        return tpmob;
+    }
+
+    /**
+     * @return the tphm
+     */
+    public String getTphm() {
+        return tphm;
+    }
+
+    
+   
+    public boolean isSms() {
+        return sms;
+    }
+
+    
+    public void setSms(boolean sms) {
+        this.sms = sms;
+    }
+
+    /**
+     * @return the color
+     */
+    public int getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    /**
+     * @return the select
+     */
+    public String getSelect() {
+        return select;
+    }
+
+    /**
+     * @param select the select to set
+     */
+    public void setSelect(String select) {
+        this.select = select;
+    }
+
+    /**
+     * @return the month2sms
+     */
+    public String getMonth2sms() {
+        return month2sms;
+    }
+
+    /**
+     * @param month2sms the month2sms to set
+     */
+    public void setMonth2sms(String month2sms) {
+        this.month2sms = month2sms;
+    }
+
+    /**
+     * @return the month6sms
+     */
+    public String getMonth6sms() {
+        return month6sms;
+    }
+
+    /**
+     * @param month6sms the month6sms to set
+     */
+    public void setMonth6sms(String month6sms) {
+        this.month6sms = month6sms;
+    }
+
+    /**
+     * @return the month9sms
+     */
+    public String getMonth9sms() {
+        return month9sms;
+    }
+
+    /**
+     * @param month9sms the month9sms to set
+     */
+    public void setMonth9sms(String month9sms) {
+        this.month9sms = month9sms;
+    }
+
+    /**
+     * @return the year1sms
+     */
+    public String getYear1sms() {
+        return year1sms;
+    }
+
+    /**
+     * @param year1sms the year1sms to set
+     */
+    public void setYear1sms(String year1sms) {
+        this.year1sms = year1sms;
+    }
+
+    /**
+     * @return the month18sms
+     */
+    public String getMonth18sms() {
+        return month18sms;
+    }
+
+    /**
+     * @param month18sms the month18sms to set
+     */
+    public void setMonth18sms(String month18sms) {
+        this.month18sms = month18sms;
+    }
+
+    /**
+     * @return the year3sms
+     */
+    public String getYear3sms() {
+        return year3sms;
+    }
+
+    /**
+     * @param year3sms the year3sms to set
+     */
+    public void setYear3sms(String year3sms) {
+        this.year3sms = year3sms;
+    }
+
+    /**
+     * @return the year5sms
+     */
+    public String getYear5sms() {
+        return year5sms;
+    }
+
+    /**
+     * @param year5sms the year5sms to set
+     */
+    public void setYear5sms(String year5sms) {
+        this.year5sms = year5sms;
+    }
+
+    /**
+     * @return the year12sms
+     */
+    public String getYear12sms() {
+        return year12sms;
+    }
+
+    /**
+     * @param year12sms the year12sms to set
+     */
+    public void setYear12sms(String year12sms) {
+        this.year12sms = year12sms;
+    }
+
+    /**
+     * @return the month4sms
+     */
+    public String getMonth4sms() {
+        return month4sms;
+    }
+
+    /**
+     * @param month4sms the month4sms to set
+     */
+    public void setMonth4sms(String month4sms) {
+        this.month4sms = month4sms;
+    }
+
+    /**
+     * @return the babyname
+     */
+    public String getBabyname() {
+        return babyname;
+    }
+
+    /**
+     * @param babyname the babyname to set
+     */
+    public void setBabyname(String babyname) {
+        this.babyname = babyname;
+    }
+
+    /**
+     * @return the mothername
+     */
+    public String getMothername() {
+        return mothername;
+    }
+
+    /**
+     * @param mothername the mothername to set
+     */
+    public void setMothername(String mothername) {
+        this.mothername = mothername;
+    }
+
+    /**
+     * @return the idnum
+     */
+    public String getIdnum() {
+        return idnum;
+    }
+
+    /**
+     * @param idnum the idnum to set
+     */
+    public void setIdnum(String idnum) {
+        this.idnum = idnum;
+    }
+}
