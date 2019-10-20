8bac40e2ade0ea26b8de3a1160cbdb28f5de9717
Nimesha Weerasinghe <nimeshaweerasinghe94@gmail.com>
2019-10-20 10:26:57 +0530

    file

---
+++ src/medical/Medical.java
@@ -0,0 +1,18 @@
+
+package medical;
+
+import javax.swing.JFrame;
+
+
+public class Medical {
+
+   
+    public static void main(String[] args) {
+     
+        
+          MainFrame mf=new MainFrame();
+        mf.setVisible(true);
+        mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+}
