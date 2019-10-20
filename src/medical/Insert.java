55c599825b55f7aa26d70c221dd711584cc80ba3
Nimesha Weerasinghe <nimeshaweerasinghe94@gmail.com>
2019-10-20 10:23:41 +0530

    file

---
+++ src/medical/Insert.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package medical;
+
+import com.mysql.jdbc.PreparedStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Hansi
+ */
+public class Insert {
+  
+    ArrayList<AgeInfo> getDetailses1(AgeInfo ag) {
+    ArrayList<AgeInfo> list=new ArrayList<AgeInfo>();
+    list.add(ag);
+    return list;}
+    
+    
+     ArrayList<AgeInfo> getDetailses2(AgeInfo ag) {
+    ArrayList<AgeInfo> list=new ArrayList<AgeInfo>();
+    list.add(ag);
+    return list;}
+    
+     ArrayList<AgeInfo> getDetailses3(AgeInfo ag) {
+    ArrayList<AgeInfo> list=new ArrayList<AgeInfo>();
+    list.add(ag);
+    return list;}
+    
+     ArrayList<AgeInfo> getDetailses4(AgeInfo ag) {
+    ArrayList<AgeInfo> list=new ArrayList<AgeInfo>();
+    list.add(ag);
+    return list;}
+    
+     ArrayList<AgeInfo> getDetailses5(AgeInfo ag) {
+    ArrayList<AgeInfo> list=new ArrayList<AgeInfo>();
+    list.add(ag);
+    return list;}
+    
+     ArrayList<AgeInfo> getDetailses6(AgeInfo ag) {
+    ArrayList<AgeInfo> list=new ArrayList<AgeInfo>();
+    list.add(ag);
+    return list;}
+    
+     ArrayList<AgeInfo> getDetailses7(AgeInfo ag) {
+    ArrayList<AgeInfo> list=new ArrayList<AgeInfo>();
+    list.add(ag);
+    return list;}
+    
+     ArrayList<AgeInfo> getDetailses8(AgeInfo ag) {
+    ArrayList<AgeInfo> list=new ArrayList<AgeInfo>();
+    list.add(ag);
+    return list;}
+    
+    
+    
+   
+   
+   
+}
